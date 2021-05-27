02cbbbfcbd2478f7b91bd8a55a744beb100b87ba
yoonho <yoonho0922@naver.com>
2021-05-27 12:10:16 +0900

    init: spring boot

---
+++ SpringBoot/SpringBoot-API/src/main/java/ac/kr/smu/springboot/BoardApplication.java
@@ -0,0 +1,11 @@
+package ac.kr.smu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class,args);
+    }
+}
