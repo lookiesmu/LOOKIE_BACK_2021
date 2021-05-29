f1cea8cdacf679ccea2d8cc4ee56b307582cd61c
yoonho <yoonho0922@naver.com>
2021-05-29 21:19:10 +0900

    Spring Boot-3

--- SpringBoot/SpringBoot-API/src/main/java/ac/kr/smu/controller/PostController.java
+++ SpringBoot/SpringBoot-API/src/main/java/ac/kr/smu/controller/PostController.java
@@ -1,12 +1,15 @@
 package ac.kr.smu.controller;
 
+import ac.kr.smu.assembler.PostAssembler;
 import ac.kr.smu.domain.entity.Post;
 import ac.kr.smu.domain.repository.PostRepository;
+import ac.kr.smu.resource.PostResource;
 import ac.kr.smu.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,13 +24,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/post")
 public class PostController {
     private final PostService postService;
+    private final PostAssembler postAssembler;
 
     @GetMapping
     public ResponseEntity<?> getPost(@PageableDefault Pageable pageable){
         Page<Post> postList = postService.getPostList(pageable);
+        CollectionModel<PostResource> collectionModel = postAssembler.toCollectionModel(postList);
         PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(pageable.getPageSize(),
                 postList.getNumber(),postList.getTotalElements());
-        PagedModel<Post> body = PagedModel.of(postList.getContent(),pageMetadata);
+        PagedModel<PostResource> body = PagedModel.of(collectionModel.getContent(),pageMetadata);
 
         body.add(linkTo(methodOn(PostController.class).getPost(pageable)).withSelfRel());
         return ResponseEntity.ok(body);
